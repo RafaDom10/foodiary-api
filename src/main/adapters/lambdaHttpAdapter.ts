56796dd4d49c87e314e9c7a8865f40bb4f4ad57f
Rafael Domingues <rafael.domingues@confirm8.com>
2025-06-11 10:56:56 -0300

    feat: add custom http errors

--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -2,6 +2,9 @@ import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { lambdaBodyParser } from '../utils/lambdaBodyParser';
 import { Controller } from '../../application/contracts/Controller';
 import { ZodError } from 'zod';
+import { ErrorCode } from '../../application/errors/ErrorCode';
+import { lambdaErrorResponse } from '../utils/lambdaErrorResponse';
+import { HttpError } from '../../application/errors/http/HttpError';
 
 export function lambdaHttpAdapter(controller: Controller<unknown>) {
   return async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
@@ -20,31 +23,28 @@ export function lambdaHttpAdapter(controller: Controller<unknown>) {
         statusCode: result.statusCode,
         body: result.body ? JSON.stringify(result.body) : undefined,
       };
-    } catch ( error ) {
+
+    } catch (error) {
       if (error instanceof ZodError) {
-        return {
+        return lambdaErrorResponse({
           statusCode: 400,
-          body: JSON.stringify({
-            error: {
-              code: 'VALIDATION',
-              message: error.issues.map( issue => ({
-                field: issue.path.join('.'),
-                error: issue.message,
-              })),
-            },
-          }),
-        };
+          code: ErrorCode.VALIDATION,
+          message: error.issues.map(issue => ({
+            field: issue.path.join('.'),
+            error: issue.message,
+          })),
+        });
       }
 
-      return {
-          statusCode: 500,
-          body: JSON.stringify({
-            error: {
-              code: 'INTERNAL_SERVER_ERROR',
-              message: 'Internal server error',
-            },
-          }),
-        };
+      if (error instanceof HttpError) {
+        return lambdaErrorResponse(error);
+      }
+
+      return lambdaErrorResponse({
+        statusCode: 500,
+        code: ErrorCode.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error',
+      });
     }
   };
 }
